2d91e2995629017449eb9a9c1ebe448a4ae7c97c
7thmaniac <varanasidbzt@gmail.com>
2024-08-12 02:19:14 -0400

    Diagonal Star

--- DiagonalStar/src/DiagonalStar.java
+++ DiagonalStar/src/DiagonalStar.java
@@ -1,7 +1,25 @@
 public class DiagonalStar {
     public static void main(String[] args) {
-
+        printSquareStar(5);
+        printSquareStar(6);
+        printSquareStar(7);
     }
 
-    
+    public static void printSquareStar(int n) {
+        if (n < 5){
+            System.out.println("Invalid Value");
+            return;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n; j++){
+                if(i == 1 || j==1 || i == n || j == n || i == j || j == n - (i-1)){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
