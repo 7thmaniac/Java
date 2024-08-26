d3f4476e80bcf6ba6081794ae12427352ca75263
7thmaniac <varanasidbzt@gmail.com>
2024-08-26 16:02:00 -0400

    Composition

---
+++ Composition/src/Ceiling.java
@@ -0,0 +1,19 @@
+public class Ceiling {
+    public int getHeight() {
+        return height;
+    }
+
+    public int getPaintedColor() {
+        return paintedColor;
+    }
+
+    private int height;
+    private int paintedColor;
+
+    public Ceiling(int height, int paintedColor) {
+        this.height = height;
+        this.paintedColor = paintedColor;
+    }
+
+
+}
